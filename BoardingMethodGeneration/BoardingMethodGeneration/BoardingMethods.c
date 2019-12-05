aa9a4414e4e2dd7dd1adaf8f1ecd31288c3539ff
Laggus <53914190+Laggus@users.noreply.github.com>
2019-12-05 18:50:17 +0100

    Minor structual change to boarding method generation

--- BoardingMethodGeneration/BoardingMethodGeneration/BoardingMethods.c
+++ BoardingMethodGeneration/BoardingMethodGeneration/BoardingMethods.c
@@ -19,34 +19,20 @@ FILE* SetBoardingMethod(BoardingInfo* _BI, FILE* _FP, int _Index) {
 }
 
 void GetSeatNumber(BoardingInfo* _BI, int _SeatIndex, char _SeatString[]) {
+	int (*GetSeat)(BoardingInfo*, int);
 	switch (_BI->BoardingMethod.BoardingMethod) {
-	case BMRandom:
-		// Either 1 for first class or 2 for peasent class
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForRandom(_BI, _SeatIndex));
-		break;
-	case BMWilma:
-		// Adds one index per seat between the current seat and the window
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForWilma(_BI, _SeatIndex));
-		break;
-	case BMFrontToBack:
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForFrontToBack(_BI, _SeatIndex));
-		break;
-	case BMBackToFront:
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForBackToFront(_BI, _SeatIndex));
-		break;
-	case BMSteffenModified:
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForSteffenModified(_BI, _SeatIndex));
-		break;
-	case BMJanModified:
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForJanModified(_BI, _SeatIndex));
-		break;
-	case BMSteffenPerfect:
-		sprintf_s(_SeatString, 10, "%d,", GetSeatForSteffenPerfect(_BI, _SeatIndex));
-		break;
+	case BMRandom:			GetSeat = GetSeatForRandom;				break;
+	case BMWilma:			GetSeat = GetSeatForWilma;				break;
+	case BMFrontToBack:		GetSeat = GetSeatForFrontToBack;		break;
+	case BMBackToFront:		GetSeat = GetSeatForBackToFront;		break;
+	case BMSteffenModified:	GetSeat = GetSeatForSteffenModified;	break;
+	case BMJanModified:		GetSeat = GetSeatForJanModified;		break;
+	case BMSteffenPerfect:	GetSeat = GetSeatForSteffenPerfect;		break;
 	default:
-		sprintf_s(_SeatString, 10, "%d,", -1);
-		break;
+		printf("Setting seat for non existing boarding method: %d %s\n", _BI->BoardingMethod.BoardingMethod, _BI->BoardingMethod.FileName);
+		return;
 	}
+	sprintf_s(_SeatString, 10, "%d,", GetSeat(_BI, _SeatIndex));
 }
 
 int GetPeasentOffset(BoardingInfo _BI) {
