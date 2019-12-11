d2e42ddaac8c06e3506d1f7d8397c14e7f0f010a
Henrik Ginnerup <hginnerup@hotmail.com>
2019-12-11 21:37:57 +0100

    R

--- RGraphs/main.R
+++ RGraphs/main.R
@@ -4,43 +4,50 @@
 #install.packages('ggplot2', dependencies = TRUE)
 #install.packages('data.table', dependencies = TRUE)
 #install.packages("tidyverse")
-#library(tidyverse)
+library(tidyverse)
 library(ggplot2)
-
+library(dplyr)
+library(purrr)
 
 #Import Data #https://stackoverflow.com/questions/30054765/how-to-get-r-to-read-in-files-from-multiple-subdirectories-under-one-large-direc
-datafiles = list.files(path="data", recursive=TRUE, full.names=TRUE)
-data = {}
-for(file in datafiles) {
-  data = append(data, read.csv(file));
-}
+#datafiles = list.files(path="data", recursive=TRUE, full.names=TRUE)
+#data = {}
+#for(file in datafiles) {
+#  data = append(data, read.csv(file));
+#}
+#data = read.csv("/data/outputfile.csv")
 #temp = list.files(pattern="*.csv")
 #data = sapply(datafiles, read.delim)
+
+
+data = read.csv("data/outputfile.csv");
 
 print(data)
 
 groupedData = data %>%
-  unlist() %>%
+#  unlist() %>%
   group_by(boarding.method, walk.speed.0, luggage.count.0);
 
-#groupedData = 
-groupedData %>% transform(
+groupedData = groupedData %>% transform(
   Min = pmin(Iterations),
   Max = pmax(Iterations),
-  Mean = sapply(groupedData$Iterations, mean),
-  graphId = boarding.method,
+  #  Mean = sapply(groupedData$Iterations, mean),
+  #Mean = mean(groupedData$Iterations),
+  graphId = boarding.method
 );
 
-for(iterations in groupedData) {
-  print(length(iterations));
-}
+groupedData = groupedData[ which(groupedData$boarding.method=="BoardingMethods/steffenmodified.txt"),]
+groupedData = groupedData[ which(groupedData$walk.speed.0==1),]
+#groupedData = groupedData[ which(groupedData$luggage.count.0),]
 
-Mean = groupedData$Iterations %>% sapply(mean)
+Mean = sapply(groupedData$Iterations, mean);
 
 # https://stackoverflow.com/questions/25244241/line-plot-with-average-and-shadow-for-min-max
 ggplot(groupedData) +
-  geom_line(  aes(x = groupedData$luggage.count.0, y=Mean))# +
-  geom_ribbon(aes(x = groupedData$luggage.count.0, ymax = Max, ymin = Min), alpha = 0.6, fill = "skyblue")
+  geom_line(  aes(x=luggage.count.0, y=Mean)) +
+  #geom_ribbon(aes(x=luggage.count.0, ymax=Max, ymin=Min), alpha = 0.6, fill = "skyblue") + 
+  geom_ribbon(aes(x=luggage.count.0, ymax=Max, ymin=Min), alpha = 0.6, fill = "red")
+
 
 
 
