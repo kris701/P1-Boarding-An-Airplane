52fc019b803200bf241219e441768f863e048f95
Henrik Ginnerup <hginnerup@hotmail.com>
2019-12-12 13:55:09 +0100

    Split data depending on assigned to door or not

--- RGraphs/main.R
+++ RGraphs/main.R
@@ -13,18 +13,21 @@ library(extrafont)
 
 data = read.csv("data/output.csv");
 
-displayData <- data %>% 
+displayData = data %>% 
   group_by(
     boarding.method,
     luggage.count.0,
-    walk.speed.0,
-    ) %>% 
+    walk.speed.0
+  ) %>% 
   summarise(
-    IteMean = mean(Iterations), 
-    IteMax = max(Iterations), 
-    IteMin = min(Iterations),
-#    WalkLabel = paste(walk.speed.0, " walking speed")
-  );
+    IteMeanAssignedDoor = mean(Iterations[assign.to.nearest.door==1]),
+    IteMaxAssignedDoor  = max( Iterations[assign.to.nearest.door==1]),
+    IteMinAssignedDoor  = min( Iterations[assign.to.nearest.door==1]),
+    
+    IteMeanUnassignedDoor = mean(Iterations[assign.to.nearest.door==0]),
+    IteMaxUnassignedDoor  = max( Iterations[assign.to.nearest.door==0]),
+    IteMinUnassignedDoor  = min( Iterations[assign.to.nearest.door==0])
+);
 
 displayData$boardingName <- factor(displayData$boarding.method, 
    levels = c( 
@@ -58,25 +61,26 @@ displayData$boardingName <- factor(displayData$boarding.method,
    )
 );
 
-displayData$walkLabel = factor(displayData$walk.speed.0,
-                               levels = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29),
-                               label  = c("0 walking speed", "1 walking speed", "2 walking speed", "3 walking speed", "4 walking speed", "5 walking speed", "6 walking speed", "7 walking speed", "8 walking speed", "9 walking speed", "10 walking speed", "11 walking speed", "12 walking speed", "13 walking speed", "14 walking speed", "15 walking speed", "16 walking speed", "17 walking speed", "18 walking speed", "19 walking speed", "20 walking speed", "21 walking speed", "22 walking speed", "23 walking speed", "24 walking speed", "25 walking speed", "26 walking speed", "27 walking speed", "28 walking speed", "29 walking speed")
-)
-
 displayData$walkLabel <- sprintf( "%s walking speed", displayData$walk.speed.0 )
-
 
 yMax = max(data$Iterations);
 yMin = min(data$Iterations);
 
-
 limitedData = displayData %>% subset(boarding.method == "BoardingMethods/backtofront.txt")
-limitedData %>% ggplot(aes(x = luggage.count.0, y=IteMean)) +
+limitedData %>% ggplot() +
   ylim(yMin, yMax) +
-  geom_point() + geom_line() +
-  geom_ribbon(aes(x=luggage.count.0, ymax=IteMax, ymin=IteMin), alpha = 0.6, fill = "skyblue") +
+  geom_ribbon(aes(x=luggage.count.0, ymax=IteMaxAssignedDoor  , ymin=IteMinAssignedDoor),   alpha = 0.5, fill = "blue") +
+  geom_ribbon(aes(x=luggage.count.0, ymax=IteMaxUnassignedDoor, ymin=IteMinUnassignedDoor), alpha = 0.5, fill = "orange") +
+  
+  geom_line(aes(x = luggage.count.0, y=IteMeanAssignedDoor), color="blue") +
+  geom_point(aes(x = luggage.count.0, y=IteMeanAssignedDoor), color="blue") +
+
+  geom_line(aes(x = luggage.count.0, y=IteMeanUnassignedDoor), color="red") +
+  geom_point(aes(x = luggage.count.0, y=IteMeanUnassignedDoor), color="red") +
+
   facet_wrap(~walkLabel) +
   labs(title = limitedData$boardingName, x="Luggage Count", y="Average Iterations")
+
 
 
 for(iteratedMethod in unique(displayData$boarding.method)) {
@@ -90,8 +94,7 @@ for(iteratedMethod in unique(displayData$boarding.method)) {
     facet_wrap(~walkLabel) +
     labs(title = limitedData$boardingName, x="Luggage Count", y="Average Iterations")
   
-  
-  ggsave(paste(iteratedMethod,".pdf"), plot = generatedPlot, device = "pdf", path = "plots",
+  ggsave(paste(iteratedMethod,".pdf", sep = ""), plot = generatedPlot, device = "pdf", path = "plots",
          scale = 1, width = 20, height = 10, units = "cm",
          dpi = 300, limitsize = TRUE)
 }
