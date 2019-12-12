6875be88c6a4ab4ea14d9c4aecebb953fe33702c
Henrik Ginnerup <hginnerup@hotmail.com>
2019-12-12 11:03:07 +0100

    Takes all methods from CSV and plots to different PDFs

--- RGraphs/main.R
+++ RGraphs/main.R
@@ -9,13 +9,13 @@ library(ggplot2)
 library(dplyr)
 library(purrr)
 
-data = read.csv("data/outputfile.csv");
+data = read.csv("data/output.csv");
 
 displayData <- data %>% 
   group_by(
     boarding.method,
     luggage.count.0,
-    walk.speed.0
+    walk.speed.0,
     ) %>% 
   summarise(
     IteMean = mean(Iterations), 
@@ -23,13 +23,30 @@ displayData <- data %>%
     IteMin = min(Iterations)
   );
 
-displayData %>% ggplot(aes(x = luggage.count.0, y=IteMean, color = factor(boarding.method))) + 
-  geom_point() + geom_line() +
-  geom_ribbon(aes(x=luggage.count.0, ymax=IteMax, ymin=IteMin), alpha = 0.6, fill = "skyblue") +
-  facet_wrap(~walk.speed.0)
+displayData$boardingName <- factor(displayData$boarding.method, 
+                                   levels = c(
+                                                "BoardingMethods/backtofront.txt",
+                                                "BoardingMethods/backtofront1door.txt"
+                                              ),
+                                   labels = c(
+                                     "Back to Front",
+                                     "Back to Front ( 1 door )"
+                                     )
+)
 
-
-
-
-
+for(iteratedMethod in unique(displayData$boarding.method)) {
+  
+  limitedData = displayData %>% subset(boarding.method == iteratedMethod)
+  
+  generatedPlot = limitedData %>% ggplot(aes(x = luggage.count.0, y=IteMean)) + 
+    geom_point() + geom_line() +
+    geom_ribbon(aes(x=luggage.count.0, ymax=IteMax, ymin=IteMin), alpha = 0.6, fill = "skyblue") +
+    facet_wrap(~walk.speed.0) +
+    labs(title = limitedData$boardingName, x="Luggage Count", y="Average Iterations")
+  
+  
+  ggsave(paste(iteratedMethod,".pdf"), plot = generatedPlot, device = "pdf", path = "plots",
+         scale = 1, width = 20, height = 10, units = "cm",
+         dpi = 300, limitsize = TRUE)
+}
 
